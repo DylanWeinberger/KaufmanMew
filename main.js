11ffae3a468b6f3a97fca336f071bedb35da945d
Dylan Weinberger <djweinbe@gmail.com>
2016-05-06 13:15:30 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	
+	$(".hamburger").click(function(){
+		if (!$(".nav").hasClass("navActive")){
+			$(".nav").addClass("navActive");
+		}else {
+			$(".nav").removeClass("navActive");
+		};
+
+	});
+});
