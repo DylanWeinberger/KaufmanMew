14911af80bdde7898004f41dc4d365791447a302
Dylan Weinberger <djweinbe@gmail.com>
2016-05-09 12:45:28 -0400

    Added New Logo. Created animations for slider

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-	
+	$(".leftImg").addClass("leftActive");
+	$(".rightImg").addClass("rightActive")
 	$(".hamburger").click(function(){
 		if (!$(".nav").hasClass("navActive")){
 			$(".nav").addClass("navActive");
@@ -8,4 +9,26 @@ $(document).ready(function(){
 		};
 
 	});
+
+	 function autoslide(){
+        var currentSlide = $('.active-slide');
+        var nextSlide = currentSlide.next();
+        var leftImg = $('.leftImg');
+        var rightImg = $('.rightImg');
+		
+		if(nextSlide.length === 0) {
+            nextSlide = $('.slide').first();
+        }
+
+        currentSlide.fadeOut(1000).removeClass("active-slide");
+        leftImg.removeClass("leftActive");
+        rightImg.removeClass("rightActive");
+        nextSlide.fadeIn(1000).addClass("active-slide");
+        leftImg.addClass("leftActive");
+        rightImg.addClass("rightActive");
+
+    };
+	
+	setInterval(autoslide, 5000);
+
 });
